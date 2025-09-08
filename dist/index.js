529e10051907e8db47228790bf8626accff2c48f
fish <th3fishmk@proton.me>
2025-09-08 01:28:52 -0600

    working calc

--- dist/index.js
+++ dist/index.js
@@ -1,57 +1,237 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log(`hello`);
+const nonNumeric = /[^0-9.]/;
 const currentLine = document.getElementById('current');
+const previousLine = document.getElementById('ans');
+let pendingOperation;
 // Digits
-const digit0 = document.getElementById('digit0');
-const digit1 = document.getElementById('digit1');
-const digit2 = document.getElementById('digit2');
-const digit3 = document.getElementById('digit3');
-const digit4 = document.getElementById('digit4');
-const digit5 = document.getElementById('digit5');
-const digit6 = document.getElementById('digit6');
-const digit7 = document.getElementById('digit7');
-const digit8 = document.getElementById('digit8');
-const digit9 = document.getElementById('digit9');
-const period = document.getElementById('period');
-// Clear
-const clear = document.getElementById('btn-clear');
-digit0.onclick = function () {
+const digit0Btn = document.getElementById('digit0');
+const digit1Btn = document.getElementById('digit1');
+const digit2Btn = document.getElementById('digit2');
+const digit3Btn = document.getElementById('digit3');
+const digit4Btn = document.getElementById('digit4');
+const digit5Btn = document.getElementById('digit5');
+const digit6Btn = document.getElementById('digit6');
+const digit7Btn = document.getElementById('digit7');
+const digit8Btn = document.getElementById('digit8');
+const digit9Btn = document.getElementById('digit9');
+const periodBtn = document.getElementById('period');
+digit0Btn.onclick = () => {
     insertDigit('0');
 };
-digit1.onclick = function () {
+digit1Btn.onclick = () => {
     insertDigit('1');
 };
-digit2.onclick = function () {
+digit2Btn.onclick = () => {
     insertDigit('2');
 };
-digit3.onclick = function () {
+digit3Btn.onclick = () => {
     insertDigit('3');
 };
-digit4.onclick = function () {
+digit4Btn.onclick = () => {
     insertDigit('4');
 };
-digit5.onclick = function () {
+digit5Btn.onclick = () => {
     insertDigit('5');
 };
-digit6.onclick = function () {
+digit6Btn.onclick = () => {
     insertDigit('6');
 };
-digit7.onclick = function () {
+digit7Btn.onclick = () => {
     insertDigit('7');
 };
-digit8.onclick = function () {
+digit8Btn.onclick = () => {
     insertDigit('8');
 };
-digit9.onclick = function () {
+digit9Btn.onclick = () => {
     insertDigit('9');
 };
-clear.onclick = function () {
-    currentLine.textContent = '0';
+periodBtn.onclick = () => {
+    insertDigit('.');
 };
+// Functions
+const clearBtn = document.getElementById('btn-clear');
+const backspaceBtn = document.getElementById('backspace');
+// events
+clearBtn.onclick = () => {
+    currentLine.textContent = '0';
+    previousLine.textContent = '';
+    unsetCal();
+};
+backspaceBtn.onclick = () => {
+    removeDigit();
+};
+// Math operations
+const additionBtn = document.getElementById('plus');
+const subtractionBtn = document.getElementById('minus');
+const multiplicationBtn = document.getElementById('times');
+const divisionBtn = document.getElementById('divide');
+const equalBtn = document.getElementById('equals');
+const squareBtn = document.getElementById('square');
+const squareRootBtn = document.getElementById('root');
+// events
+additionBtn.onclick = () => {
+    cacheOperation(mathOperation.addition);
+};
+subtractionBtn.onclick = () => {
+    cacheOperation(mathOperation.subtraction);
+};
+multiplicationBtn.onclick = () => {
+    cacheOperation(mathOperation.multiplication);
+};
+divisionBtn.onclick = () => {
+    cacheOperation(mathOperation.division);
+};
+squareBtn.onclick = () => {
+    cacheOperation(mathOperation.square);
+    doTheMath();
+};
+squareRootBtn.onclick = () => {
+    cacheOperation(mathOperation.squareRoot);
+    doTheMath();
+};
+equalBtn.onclick = () => {
+    doTheMath();
+};
+// Others
+const displayCachedOpe = document.getElementById('currentOperation');
 function insertDigit(digit) {
-    console.log(`You clicked ${digit}`);
     let newText = currentLine.textContent + digit;
-    currentLine.textContent = String(Number(newText));
+    if (digit === '.') {
+        if (currentLine.textContent.includes('.')) {
+            console.warn(`Ignoring period`);
+        }
+        else {
+            currentLine.textContent = String(newText);
+        }
+    }
+    else {
+        currentLine.textContent = String(Number(newText));
+    }
+}
+function removeDigit() {
+    console.log(`Removing last digit`);
+    currentLine.textContent = currentLine.textContent.slice(0, -1);
+    if (currentLine.textContent === '' || currentLine.textContent === '-') {
+        currentLine.textContent = '0';
+    }
+    else if (!currentLine.textContent.endsWith('.')) {
+        currentLine.textContent = String(Number(currentLine.textContent));
+    }
+}
+function cacheOperation(toDo) {
+    console.log(`Sending operation to cache`);
+    previousLine.textContent = currentLine.textContent;
+    currentLine.textContent = String(Number(0));
+    pendingOperation = toDo;
+    displayCachedOpe.textContent = toDo;
+}
+function doTheMath() {
+    console.log(`Doing the math`);
+    let firstNumber = Number(previousLine.textContent);
+    let secondNumber = Number(currentLine.textContent);
+    let answer = 0;
+    switch (pendingOperation) {
+        case mathOperation.addition:
+            answer = firstNumber + secondNumber;
+            previousLine.textContent = '';
+            currentLine.textContent = String(answer);
+            unsetCal();
+            break;
+        case mathOperation.subtraction:
+            answer = firstNumber - secondNumber;
+            previousLine.textContent = '';
+            currentLine.textContent = String(answer);
+            unsetCal();
+            break;
+        case mathOperation.multiplication:
+            answer = firstNumber * secondNumber;
+            previousLine.textContent = '';
+            currentLine.textContent = String(answer);
+            unsetCal();
+            break;
+        case mathOperation.division:
+            if (secondNumber === 0) {
+                console.error(`Attempt of division by zero`);
+                break;
+            }
+            answer = firstNumber / secondNumber;
+            previousLine.textContent = '';
+            currentLine.textContent = String(answer);
+            unsetCal();
+            break;
+        case mathOperation.square:
+            answer = firstNumber ** 2;
+            previousLine.textContent = '';
+            currentLine.textContent = String(answer);
+            unsetCal();
+            break;
+        case mathOperation.squareRoot:
+            answer = Math.sqrt(firstNumber);
+            previousLine.textContent = '';
+            currentLine.textContent = String(answer);
+            unsetCal();
+            break;
+        case undefined:
+            break;
+        default:
+            currentLine.textContent = 'Operation not supported';
+            previousLine.textContent = '';
+            pendingOperation = undefined;
+            unsetCal();
+            break;
+    }
+}
+var mathOperation;
+(function (mathOperation) {
+    mathOperation["addition"] = "+";
+    mathOperation["subtraction"] = "-";
+    mathOperation["multiplication"] = "*";
+    mathOperation["division"] = "/";
+    mathOperation["square"] = "^2";
+    mathOperation["squareRoot"] = "square";
+})(mathOperation || (mathOperation = {}));
+function unsetCal() {
+    pendingOperation = undefined;
+    displayCachedOpe.textContent = '';
+}
+currentLine.oninput = (e) => {
+    if (e instanceof InputEvent) {
+        if (e.data === '+') {
+            cleanNumbers();
+            cacheOperation(mathOperation.addition);
+        }
+        else if (e.data === '-') {
+            cleanNumbers();
+            cacheOperation(mathOperation.subtraction);
+        }
+        else if (e.data === '*') {
+            cleanNumbers();
+            cacheOperation(mathOperation.multiplication);
+        }
+        else if (e.data === '/') {
+            cleanNumbers();
+            cacheOperation(mathOperation.multiplication);
+        }
+        else if (e.data === '.') {
+            if (currentLine.textContent.includes('.')) {
+            }
+        }
+        else if (e.inputType === 'insertParagraph') {
+            console.log('enter detected');
+            cleanNumbers();
+            doTheMath();
+        }
+        else {
+            console.log(e);
+            cleanNumbers();
+        }
+    }
+};
+function cleanNumbers() {
+    console.log(`Cleaning the things`);
+    let onlyText = currentLine.textContent.replace(nonNumeric, '');
+    onlyText = onlyText.trim();
+    currentLine.textContent = String(Number(onlyText));
 }
 //# sourceMappingURL=index.js.map
